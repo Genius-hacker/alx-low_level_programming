e88221802588574ab76429fb3651c04618587d93
Genius-hacker <belloabdulsomod05@gmail.com>
2023-10-11 05:31:18 -0700

    iol

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,42 +1,90 @@
 #include "main.h"
 
 /**
-  * print_times_table - printsthe times inputed
-  * starting with 0.
-  *@n: the value of the timea table printed
-  */
-
+ * print_times_table - prints time table
+ * @n : use in times table
+ * Return:void
+ */
 void print_times_table(int n)
 {
+	int row = 0, rep, col;
 
-	int num, mult, prod;
+	if (n < 0 || n > 15)
+		return;
 
-	if (n >= 0 && n <= 15)
+	while (row <= n)
 	{
-	for (num = 0; num <= n; num++)
-	{
-	_putchar('0');
-	for (mult = 1; mult <= n; mult++)
-	{
-	_putchar(',');
-	_putchar(' ');
-	prod = num * mult;
-	if (prod <= 99)
-	_putchar(' ');
-	if (prod <= 9)
-	_putchar(' ');
-	if (prod >= 100)
-	{
-	_putchar((prod / 100) + '0');
-	_putchar(((prod / 10)) % 10 + '0');
-	}
-	else if (prod <= 99 && prod >= 10)
-	{
-	_putchar((prod / 10) + '0');
-	}
-	_putchar((prod % 10) + '0');
-	}
-	_putchar('\n');
-	}
+		for (col = 0; col <= n; col++)
+		{
+			rep = row * col;
+			if (col == 0)
+				_putchar('0' + rep);
+			else if (rep < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + rep);
+			}
+			else if (rep < 100)
+			{
+				_putchar (' ');
+				_putchar('0' + rep / 10);
+				_putchar('0' + rep % 10);
+			}
+			else
+			{
+				_putchar('0' + rep / 100);
+				_putchar('0' + (rep - 100) / 10);
+				_putchar('0' + rep % 10);
+			}
+			if (col < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+		row++;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,2 @@
-the hardest commit of mine
+i cant get this task, have been tyr
+trying for the past few days
